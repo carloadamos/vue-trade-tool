7784f92313a87983df342ca0c766f7166bc6e838
carloadamos <johncarloadamos@gmail.com>
2021-11-01 19:22:47 +0800

    :sparkles:

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col md:flex-row bg-gray-900">
+    <div class="flex-col w-full md:w-64 px-4 text-gray-200">
+      <div class="flex w-full flex-row justify-between px-4 py-4">
+        <a href="#" class="uppercase text-lg font-semibold tracking-widest">
+          TradeTool UI</a
+        >
+        <button class="rounded-lg md:hidden focus:outline-none">X</button>
+      </div>
+      <nav class="hidden md:block flex-grow">
+        <NuxtLink
+          class="block px-4 text-sm py-2 mt-2 hover:bg-gray-700 rounded-lg"
+          to="/"
+          >Dashboard</NuxtLink
+        >
+        <NuxtLink
+          class="block px-4 text-sm py-2 mt-2 hover:bg-gray-700 rounded-lg"
+          to="/trade"
+          >Trade</NuxtLink
+        >
+      </nav>
+    </div>
+    <div class="flex-grow">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.nuxt-link-exact-active {
+  background: #374151;
+}
+</style>
---
+++ pages/trade.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    class="flex justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <table class="w-full">
+      <thead>
+        <tr
+          class="
+            text-md
+            font-semibold
+            tracking-wide
+            text-left text-gray-900
+            bg-gray-100
+            uppercase
+            border-b border-gray-600
+          "
+        >
+          <th class="border px-4 py-3">Status</th>
+          <th class="border px-4 py-3">Symbol</th>
+          <th class="border px-4 py-3">Date Bought</th>
+          <th class="border px-4 py-3">Buy Price</th>
+          <th class="border px-4 py-3">Stoploss</th>
+          <th class="border px-4 py-3">Target Price</th>
+          <th class="border px-4 py-3">Sold Price</th>
+          <th class="border px-4 py-3">Date Sold</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="border px-4 py-3">Win</td>
+          <td class="border px-4 py-3">BTCUSDT</td>
+          <td class="border px-4 py-3">October 25, 2021</td>
+          <td class="border px-4 py-3">100</td>
+          <td class="border px-4 py-3">80</td>
+          <td class="border px-4 py-3">200</td>
+          <td class="border px-4 py-3">196</td>
+          <td class="border px-4 py-3">October 30, 2021</td>
+        </tr>
+
+        <tr>
+          <td class="border px-4 py-3">Loss</td>
+          <td class="border px-4 py-3">BTCUSDT</td>
+          <td class="border px-4 py-3">October 25, 2021</td>
+          <td class="border px-4 py-3">100</td>
+          <td class="border px-4 py-3">80</td>
+          <td class="border px-4 py-3">200</td>
+          <td class="border px-4 py-3">196</td>
+          <td class="border px-4 py-3">October 30, 2021</td>
+        </tr>
+        <tr>
+          <td class="border px-4 py-3">Breakeven</td>
+          <td class="border px-4 py-3">BTCUSDT</td>
+          <td class="border px-4 py-3">October 25, 2021</td>
+          <td class="border px-4 py-3">100</td>
+          <td class="border px-4 py-3">80</td>
+          <td class="border px-4 py-3">200</td>
+          <td class="border px-4 py-3">196</td>
+          <td class="border px-4 py-3">October 30, 2021</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
